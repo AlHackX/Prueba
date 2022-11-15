6f7a507d29784794147f3e34e49aae7411393c83
Ed <Ed@DESKTOP-N9GN8OD>
2022-11-15 12:46:10 -0600

    Prueba número 1

---
+++ src/ejemplogithub/EjemploGithub.java
@@ -0,0 +1,13 @@
+package ejemplogithub;
+/**
+ *
+ * @author Ed
+ */
+public class EjemploGithub {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Alan Antonio Ruacho Domínguez.");
+    }
+    
+}
